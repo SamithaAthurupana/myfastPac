3188c3b48beb092cb08f9c45b37135b051bb7044
SamithaAthurupana <athurupana1998@gmail.com>
2025-11-02 06:31:29 +0530

    def get_full_name

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
-def get_full_name(first_name, last_name):
-    full_name = first_name.title() + " " + last_name.title()
+def get_full_name(first_name:str, last_name:str):
+    full_name = first_name.title()+" "+last_name.title()
     return full_name
 
 
 
 
 if __name__ == '__main__':
-    print(get_full_name(first_name="samitha",last_name="athurupana"))
+    print(get_full_name("samitha","athurupana"))
